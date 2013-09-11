b3aae76e088476b205889452b90df8ba80cc2e07
Jakub <jakub@jakub-Aspire-5755G.(none)>
2013-09-10 19:59:45 -0700

    fixed the string problem

--- Assignment02/src/main.cpp
+++ Assignment02/src/main.cpp
@@ -15,6 +15,7 @@ struct Vertex {
     GLfloat color[3];
 };
 
+// Precious Globals
 int w = 640, h = 480;
 bool rotate = true;
 bool back = false;
@@ -34,7 +35,7 @@ void render();
 void update();
 void reshape(int, int);
 void keyboard(unsigned char, int, int);
-void mymenu(int);
+void menu(int);
 void mouseClicks(int, int, int, int);
 bool initialize();
 void cleanUp();
@@ -42,6 +43,7 @@ float getDT();
 std::chrono::time_point<std::chrono::high_resolution_clock> t1,t2;
 
 int main(int argc, char **argv) {
+	// Initialize things
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(w, h);
@@ -54,12 +56,12 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-	glutCreateMenu(mymenu);
+	// create menu and entries along with mouse and keyboard
+	glutCreateMenu(menu);
 	glutAddMenuEntry("Toggle Rotate", 1);
 	glutAddMenuEntry("Exit Program", 2);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 	glutMouseFunc(mouseClicks);
-
     glutDisplayFunc(render);
     glutReshapeFunc(reshape);
     glutIdleFunc(update);
@@ -76,14 +78,17 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void mymenu(int value) {
+// my menu function
+void menu(int value) {
 	if(value == 1)
 		rotate = !rotate;
 	if(value == 2)
 		exit(0);
 }
 
+// my mouse click function
 void mouseClicks(int button, int state, int x, int y) {
+	// if left button is pressed then rotate backwards
 	if((button == GLUT_LEFT_BUTTON) && (state == GLUT_DOWN)) {
 		back = !back;
 	}
@@ -93,18 +98,29 @@ void render() {
     glClearColor(0.0, 0.0, 0.2, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	// the mvp matrix
     mvp = projection * view * model;
 
     glUseProgram(program);
     glUniformMatrix4fv(loc_mvpmat, 1, GL_FALSE, glm::value_ptr(mvp));
 
+	// magic
     glEnableVertexAttribArray(loc_position);
     glEnableVertexAttribArray(loc_color);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo_geometry);
 
-    glVertexAttribPointer(loc_position, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-    glVertexAttribPointer(loc_color, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,color));
+    glVertexAttribPointer(loc_position, 
+						  3,
+						  GL_FLOAT, GL_FALSE,
+						  sizeof(Vertex),
+						  0);
+    glVertexAttribPointer(loc_color,
+						  3,
+						  GL_FLOAT,
+						  GL_FALSE,
+						  sizeof(Vertex),
+						  (void*)offsetof(Vertex,color));
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
     glDisableVertexAttribArray(loc_position);
@@ -121,8 +137,11 @@ void update() {
     model = glm::translate( glm::mat4(1.0f), glm::vec3(4.0 * sin(angle), 0.0, 4.0 * cos(angle)));
 	model = glm::rotate( model, lastRotate, glm::vec3(0.0f, 1.0f, 0.0f));
 
+	// check for rotate flag
 	if(rotate) {
 		model = glm::rotate( model, lastRotate, glm::vec3(0.0f, 1.0f, 0.0f));
+		
+		// check for which direction to rotate
 		if(!back) {
 			lastRotate += (dt * M_PI/2)*100.0f;
 		} else {
@@ -136,12 +155,12 @@ void update() {
 void reshape(int n_w, int n_h) {
     w = n_w;
     h = n_h;
-
     glViewport( 0, 0, w, h);
     projection = glm::perspective(45.0f, float(w)/float(h), 0.01f, 100.0f);
 }
 
 void keyboard(unsigned char key, int x_pos, int y_pos) {
+	// if esc then exit else if a then rotate backwards
     if(key == 27) {
         exit(0);
     } else if(key == 'a') {
@@ -156,12 +175,14 @@ bool shaderLoader(string fvs, string ffs){
 	std::ifstream is2;
 	int length = 0;
 
+	// load the shaders
 	is1.open(fvs, std::ios::binary);
 	is1.seekg(0, std::ios::end);
 	length = is1.tellg();
 	is1.seekg(0, std::ios::beg);
-	char *ret = new char[length];
+	char *ret = new char[length+1];
 	is1.read(ret, length);
+	ret[length+1] = '\0';
 	is1.close();
 	const char *vs = ret;
 
@@ -169,13 +190,15 @@ bool shaderLoader(string fvs, string ffs){
 	is2.seekg(0, std::ios::end);
 	length = is2.tellg();
 	is2.seekg(0, std::ios::beg);
-	char *ret2 = new char[length];
+	char *ret2 = new char[length+1];
 	is2.read(ret2, length);
+	ret2[length+1] = '\0';
 	is2.close();
 	const char *fs = ret2;
 
     GLint shader_status;
 
+	// compile vertex shader
     glShaderSource(vertex_shader, 1, &vs, NULL);
     glCompileShader(vertex_shader);
     glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &shader_status);
@@ -184,6 +207,7 @@ bool shaderLoader(string fvs, string ffs){
         return false;
     }
 
+	// compile fragment shader
     glShaderSource(fragment_shader, 1, &fs, NULL);
     glCompileShader(fragment_shader);
     glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &shader_status);
@@ -192,6 +216,7 @@ bool shaderLoader(string fvs, string ffs){
         return false;
     }
 
+	// create the program and attach the shaders
     program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
@@ -207,6 +232,7 @@ bool shaderLoader(string fvs, string ffs){
 	return true;
 }
 
+// more magic
 bool initialize() {
     Vertex geometry[] = { {{-1.0, -1.0, -1.0}, {0.0, 0.0, 0.0}},
                           {{-1.0, -1.0, 1.0}, {0.0, 0.0, 1.0}},
@@ -292,11 +318,13 @@ bool initialize() {
     return true;
 }
 
+// Delete things
 void cleanUp() {
     glDeleteProgram(program);
     glDeleteBuffers(1, &vbo_geometry);
 }
 
+// Time things
 float getDT() {
     float ret;
     t2 = std::chrono::high_resolution_clock::now();
